e159d788f867a87a3bdc43db2ad22b2e9cbfdbeb
simopoza <mannahri@student.1337.ma>
2023-05-24 05:44:14 +0100

    just small error 403 Forbidden

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 service mysql start
-mysql -u root -e "create database ${DB_NAME};"
-mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@% IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mariadb -u root -e "create database IF NOT EXISTS ${DB_NAME};"
+mariadb -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mariadb -u root -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
+echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 wp core download --allow-root
-cp wp-config-sample.php wp-config.php
-# wp core config --dbname=${WP_NAME} --dbuser=${WP_USER} --dbpass=${WP_PASSWORD} --dbhost=${WP_HOST} --allow-root
-wp core install --url=https://localhost --title="inception" --admin_user=${AD_USER} --admin_password=${AD_PASSWORD} --admin_email=${email} --skip-email --allow-root
+
+# wp core config --dbname=poza --dbuser=mannahri --dbpass=simopoza12345 --dbhost=mariadb --allow-root
+
+# wp core install --url=https://localhost --title="inception" --admin_user=ahmad --admin_password=onepieceisgoat123 --admin_email=mohammedannahri18@gmail.com  --skip-email --allow-root
+
+# wp user create mohammed mannahri@student.1337.ma --role=author --user_pass=dimadimaraja123 --allow-root
+
+wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${MARIADB_ROOT_PASSWORD} --dbhost=${DB_HOST} --allow-root
+
+wp core install --url=https://localhost --title="inception" --admin_user=${AD_USER} --admin_password=${AD_PASSWORD} --admin_email=${email}  --skip-email --allow-root
+
 wp user create ${WP_UR} ${WP_EMAIL} --role=author --user_pass=${WP_PW} --allow-root
+
 exec "$@"
