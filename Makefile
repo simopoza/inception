cb68f2caf69ac278db2fed6cd1ffa92773048b10
Mohammed Annahri <mannahri@c4r4p5.1337.ma>
2023-08-21 03:12:11 +0100

    push

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 COMPOSE_FILE = ./srcs/docker-compose.yml
 
 build:
-	@mkdir -p /home/mannahri/data/DB
+	@mkdir -p /home/mannahri/data/mariadb
 	@mkdir -p /home/mannahri/data/wordpress
-	@docker-compose $(COMPOSE_FILE) build
-up :
-	@docker-compose $(COMPOSE_FILE) up
+	@docker compose -f $(COMPOSE_FILE) up
+
 down :
-	@docker-compose $(COMPOSE_FILE) down
+	@docker compose -f $(COMPOSE_FILE) down
+
 clean :
+	@rm -rf /home/mannahri/data
 	@docker system prune -af
-	@docker volume rm $(docker volume ls -q)
+	@if [ -n "$$(docker volume ls -q)" ]; then docker volume rm $$(docker volume ls -q); fi
 	@docker network rm $(docker network ls -q)
+
+fclean: down clean
+
+.PHONY: clean down build re
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+
 echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
 wp core download --allow-root
 
-# wp core config --dbname=poza --dbuser=mannahri --dbpass=simopoza12345 --dbhost=mariadb --allow-root
-
-# wp core install --url=https://localhost --title="inception" --admin_user=ahmad --admin_password=onepieceisgoat123 --admin_email=mohammedannahri18@gmail.com --allow-root
-
-# wp user create mohammed mannahri@student.1337.ma --role=author --user_pass=dimadimaraja123 --allow-root
-
 wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${MARIADB_ROOT_PASSWORD} --dbhost=${DB_HOST} --allow-root
 
-wp core install --url=https://localhost --title="inception" --admin_user=${AD_USER} --admin_password=${AD_PASSWORD} --admin_email=${email}  --skip-email --allow-root
+wp core install --url=${URL} --title="inception" --admin_user=${AD_USER} --admin_password=${AD_PASSWORD} --admin_email=${email}  --skip-email --allow-root
 
 wp user create ${WP_UR} ${WP_EMAIL} --role=author --user_pass=${WP_PW} --allow-root
 
